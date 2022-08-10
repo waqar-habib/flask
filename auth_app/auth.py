a3bc9d27530b9a7b101693dbc801cce35ef072ef
Waqar Habib <waqarhabib94@gmail.com>
2022-08-10 17:46:30 -0500

    test code

--- auth_app/auth.py
+++ auth_app/auth.py
@@ -1,20 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # define "auth.py" as blueprint of app
 auth = Blueprint('auth', __name__)
 
 #define login, logout, sign up using render_template
-#ln 12: can pass in variable as a 2nd argument in render_template function, use it in any other page
+#can pass in variable as a 2nd argument in render_template function, use it in any other page
 
-@auth.route('/login')
+# The route below can accept get and post methods
+@auth.route('/login', methods=['GET', 'POST'])
+
 def login():
+
+    # has all data that was sent as a part of this form
+    data = request.form
+
+    # prints immutable dict with info that was filled out in the form
+    print(data)
+    
     # boolean attribute can allow if statements in other pages
-    return render_template("login.html")
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
     return render_template("sign_up.html")
