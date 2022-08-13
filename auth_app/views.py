28db7a14a37b443db5f788adfcf774d188a2197b
Waqar Habib <waqarhabib94@gmail.com>
2022-08-12 22:59:25 -0500

    working code: add delete notes functionality

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,7 +1,8 @@
-# 
-from crypt import methods
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from .import db
+import json
 
 # define "views.py" as blueprint of app
 views = Blueprint('views', __name__)
@@ -23,5 +24,34 @@ def news():
 @views.route('/notes', methods=['GET','POST'])
 @login_required
 def notes():
+    # publish notes to backend using POST method
+    if request.method == 'POST':
+        note = request.form.get('note')
+        # check for length of note
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
     # reference in home if current user is authenticated
     return render_template ("notes.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    # take in data from POST request
+    note = json.loads(request.data)
+    
+    #access noteId attribute
+    noteId = note['noteId']
+    # look for note that has id
+    note = Note.query.get(noteId)
+    #check if exists
+    if note:
+        # if user owns the note
+        if note.user_id == current_user.id:
+                db.session.delete(note)
+                db.session.commit()
+    return jsonify({})
